910c47437a96cc85c4f85044e1ecc651dfa3d287
Z <wow@wow.wow>
2023-01-09 22:09:32 -1000

    Fixed top layer bug

--- src/sprite_interaction_plugin.rs
+++ src/sprite_interaction_plugin.rs
@@ -7,6 +7,8 @@ impl Plugin for SpriteInteractionPlugin {
     fn build(&self, app: &mut App) {
         app
             .add_event::<ClickEvent>()
+            .add_event::<DragBeginEvent>()
+            .add_event::<DragEndEvent>()
             .add_event::<DropEvent>()
             .add_event::<HoveringEvent>()
             .add_event::<HoverBeginEvent>()
@@ -18,28 +20,9 @@ impl Plugin for SpriteInteractionPlugin {
             .add_system(handle_sprite_hover_events)
             .add_system_to_stage(CoreStage::Last, handle_sprite_end_drag)
 
-            .add_system(debug_hover_begin)
-            .add_system(debug_hover_end)
+            .add_plugin(SpriteEventDebugPlugin)
 
             ;
-    }
-}
-
-fn debug_hover_begin(
-    mut hover_begin_ev: EventReader<HoverBeginEvent>,
-) {
-    for ev in hover_begin_ev.iter() {
-        let ent = ev.ent;
-        println!("Began hovering over {ent:?}");
-    }
-}
-
-fn debug_hover_end(
-    mut hover_end_ev: EventReader<HoverEndEvent>,
-) {
-    for ev in hover_end_ev.iter() {
-        let ent = ev.ent;
-        println!("Ended hovering over {ent:?}");
     }
 }
 
@@ -54,6 +37,14 @@ pub struct HoverBeginEvent {
 }
 
 pub struct HoverEndEvent {
+    pub ent: Entity,
+}
+
+pub struct DragBeginEvent {
+    pub ent: Entity,
+}
+
+pub struct DragEndEvent {
     pub ent: Entity,
 }
 
@@ -151,11 +142,13 @@ fn handle_sprite_hover_events(
 }
 
 fn handle_sprite_begin_drag(
-    mut sprites: Query<(&Dragging, &mut Transform), (With<Sprite>, Added<Dragging>)>,
+    mut sprites: Query<(Entity, &Dragging, &mut Transform), (With<Sprite>, Added<Dragging>)>,
     mut top_layer: ResMut<TopLayer>,
+    mut begin_drag_ev: EventWriter<DragBeginEvent>
 ) {
-    for (_, mut xform) in &mut sprites {
+    for (ent, _, mut xform) in &mut sprites {
         xform.translation = xform.translation.truncate().extend(top_layer.top());
+        begin_drag_ev.send(DragBeginEvent {ent})
     }
 }
 
@@ -175,6 +168,7 @@ fn handle_sprite_end_drag(
     button_input: Res<Input<MouseButton>>,
     dragged_entities: Query<(Entity, &Dragging)>,
     mut drop_ev: EventWriter<DropEvent>,
+    mut drag_end_ev: EventWriter<DragEndEvent>
 ) {
     if !button_input.just_released(MouseButton::Left) {
         return;
@@ -186,5 +180,69 @@ fn handle_sprite_end_drag(
             drag_info: drag.clone(),
             ent,
         });
+        drag_end_ev.send(DragEndEvent { ent });
+    }
+}
+
+/************************************************************************
+                                    Debug
+*************************************************************************/   
+
+pub struct SpriteEventDebugPlugin;
+
+impl Plugin for SpriteEventDebugPlugin {
+    fn build(&self, app: &mut App) {
+        app
+        .add_system(debug_hover_begin)
+        .add_system(debug_hover_end)
+        .add_system(debug_drag_begin)
+        .add_system(debug_drag_end)
+        .add_system(debug_click)
+        ;
+    }
+}
+
+fn debug_hover_begin(
+    mut hover_begin_ev: EventReader<HoverBeginEvent>,
+) {
+    for ev in hover_begin_ev.iter() {
+        let ent = ev.ent;
+        println!("Began hovering over {ent:?}");
+    }
+}
+
+fn debug_hover_end(
+    mut hover_end_ev: EventReader<HoverEndEvent>,
+) {
+    for ev in hover_end_ev.iter() {
+        let ent = ev.ent;
+        println!("Ended hovering over {ent:?}");
+    }
+}
+
+fn debug_drag_begin (
+    mut drag_begin_ev: EventReader<DragBeginEvent>,
+) {
+    for ev in drag_begin_ev.iter() {
+        let ent = ev.ent;
+        println!("Began drag {ent:?}");
+    }
+}
+
+fn debug_drag_end (
+    mut drag_end_ev: EventReader<DragEndEvent>,
+) {
+    for ev in drag_end_ev.iter() {
+        let ent = ev.ent;
+        println!("End drag {ent:?}");
+    }
+}
+
+fn debug_click (
+    mut click_ev: EventReader<ClickEvent>,
+) {
+    for ev in click_ev.iter() {
+        let ent = ev.ent;
+        println!("Clicked {ent:?}");
     }
 }
