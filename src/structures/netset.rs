574e4c9fe862bfb75a2a63b285008e963395b1f4
Z <wow@wow.wow>
2023-03-01 22:58:38 -0700

    Cleaned up netset.

--- src/structures/netset.rs
+++ src/structures/netset.rs
@@ -38,46 +38,46 @@ impl<T> NetSet<T> where T: 'static + Send + Sync + Eq + Copy + core::hash::Hash 
         self.set.contains(tag)
     }
 
-    pub fn all(&self, tags: &[T]) -> bool {
-        for tag in tags {
-            if !self.contains(tag) {
+    pub fn contains_all_slice(&self, items: &[T]) -> bool {
+        for item in items {
+            if !self.contains(item) {
                 return false
             }
         }
         true
     }
 
-    pub fn any(&self, tags: &[T]) -> bool {
-        for tag in tags {
-            if self.contains(tag) {
+    pub fn contains_any_slice(&self, items: &[T]) -> bool {
+        for item in items {
+            if self.contains(item) {
                 return true
             }
         }
         false
     }
 
-    pub fn none(&self, tags: &[T]) -> bool {
+    pub fn contains_none_slice(&self, items: &[T]) -> bool {
         let mut res = false;
-        for tag in tags {
-            res |= self.contains(tag);
+        for item in items {
+            res |= self.contains(item);
         }
         !res
     }
 
-    pub fn all_g(&self, other_set: &NetSet<T>) -> bool {
-        self.set.is_superset(&other_set.set)
+    pub fn contains_all(&self, other_set: &NetSet<T>) -> bool {
+        other_set.set.is_subset(&self.set)
     }
 
-    pub fn any_g(&self, other_set: &NetSet<T>) -> bool {
-        for tag in &other_set.set {
-            if self.set.contains(&tag) {
+    pub fn contains_any(&self, other_set: &NetSet<T>) -> bool {
+        for item in &other_set.set {
+            if self.set.contains(&item) {
                 return true
             }
         }
         false
     }
 
-    pub fn none_g(&self, other_set: &NetSet<T>) -> bool {
+    pub fn contains_none(&self, other_set: &NetSet<T>) -> bool {
         self.set.is_disjoint(&other_set.set)
     }
 }
@@ -108,20 +108,20 @@ mod tag_tests {
         let test_tags = NetSet::<TestEnum>::make_test_set(&[TestTwo, TestOne]);
         let test_tags2 = NetSet::<TestEnum>::make_test_set(&[TestOne]);
         let test_tags3 = NetSet::<TestEnum>::make_test_set(&[TestThree]);
+        let test_tags4 = NetSet::<TestEnum>::make_test_set(&[TestTwo, TestOne, TestThree, TestFour]);
     
-        assert_eq!(test_tags.all_g(&test_tags), true, "Failed all self test.");
-        assert_eq!(test_tags.all(&[TestTwo, TestOne]), true, "Failed all TestTwo TestOne");
-        assert_eq!(test_tags.all(&[TestTwo]), true, "Failed all TestTwo");
+        assert_eq!(test_tags.contains_all(&test_tags), true, "Failed all self test.");
+        assert_eq!(test_tags.contains_all_slice(&[TestTwo, TestOne]), true, "Failed all TestTwo TestOne");
+        assert_eq!(test_tags.contains_all_slice(&[TestTwo]), true, "Failed all TestTwo");
     
-        assert_eq!(test_tags.all_g(&test_tags2), true, "Failed all test tags 1 V test tags 2");
-        assert_eq!(test_tags2.all_g(&test_tags), false, "Failed all test tags 2 V test tags 1");
-        assert_eq!(test_tags2.all(&[TestTwo, TestOne]), false, "Failed all TestTwo TestOne.");
+        assert_eq!(test_tags.contains_all(&test_tags2), true, "Failed all test tags 1 V test tags 2");
+        assert_eq!(test_tags2.contains_all(&test_tags), false, "Failed all test tags 2 V test tags 1");
+        assert_eq!(test_tags2.contains_all_slice(&[TestTwo, TestOne]), false, "Failed all TestTwo TestOne.");
     
-        assert_eq!(test_tags2.all(&[TestThree]), false, "Failed all third.");
-        assert_eq!(test_tags2.all_g(&test_tags3), false, "Failed all third g.");
+        assert_eq!(test_tags2.contains_all_slice(&[TestThree]), false, "Failed all third.");
+        assert_eq!(test_tags2.contains_all(&test_tags3), false, "Failed all third g.");
 
-        assert_eq!(test_tags.all_g(&test_tags3), false, "Failed all third g.");
-        assert_eq!(test_tags3.all_g(&test_tags), false, "Failed all third g.");
+        assert_eq!(test_tags.contains_all(&test_tags4), false, "Failed all fourth g1.");
     }
     
     #[test]
@@ -130,17 +130,20 @@ mod tag_tests {
         let test_tags = NetSet::<TestEnum>::make_test_set(&[TestTwo, TestOne]);
         let test_tags2 = NetSet::<TestEnum>::make_test_set(&[TestOne]);
         let test_tags3 = NetSet::<TestEnum>::make_test_set(&[TestThree]);
+        let test_tags4 = NetSet::<TestEnum>::make_test_set(&[TestTwo, TestOne, TestThree, TestFour]);
     
-        assert_eq!(test_tags.any_g(&test_tags), true, "Failed any self test.");
-        assert_eq!(test_tags.any(&[TestTwo, TestOne]), true, "Failed any TestTwo TestOne");
-        assert_eq!(test_tags.any(&[TestTwo]), true, "Failed any TestTwo");
+        assert_eq!(test_tags.contains_any(&test_tags), true, "Failed any self test.");
+        assert_eq!(test_tags.contains_any_slice(&[TestTwo, TestOne]), true, "Failed any TestTwo TestOne");
+        assert_eq!(test_tags.contains_any_slice(&[TestTwo]), true, "Failed any TestTwo");
     
-        assert_eq!(test_tags.any_g(&test_tags2), true, "Failed any test tags 1 V test tags 2");
-        assert_eq!(test_tags2.any_g(&test_tags), true, "Failed any test tags 2 V test tags 1");
-        assert_eq!(test_tags2.any(&[TestTwo, TestOne]), true, "Failed any TestTwo TestOne.");
+        assert_eq!(test_tags.contains_any(&test_tags2), true, "Failed any test tags 1 V test tags 2");
+        assert_eq!(test_tags2.contains_any(&test_tags), true, "Failed any test tags 2 V test tags 1");
+        assert_eq!(test_tags2.contains_any_slice(&[TestTwo, TestOne]), true, "Failed any TestTwo TestOne.");
     
-        assert_eq!(test_tags2.any(&[TestThree]), false, "Failed any third.");
-        assert_eq!(test_tags2.any_g(&test_tags3), false, "Failed any third g.");
+        assert_eq!(test_tags2.contains_any_slice(&[TestThree]), false, "Failed any third.");
+        assert_eq!(test_tags2.contains_any(&test_tags3), false, "Failed any third g.");
+
+        assert_eq!(test_tags4.contains_any(&test_tags3), true, "Failed any fourth g.");
     }
     
     #[test]
@@ -149,16 +152,19 @@ mod tag_tests {
         let test_tags = NetSet::<TestEnum>::make_test_set(&[TestTwo, TestOne]);
         let test_tags2 = NetSet::<TestEnum>::make_test_set(&[TestOne]);
         let test_tags3 = NetSet::<TestEnum>::make_test_set(&[TestThree]);
+        let test_tags4 = NetSet::<TestEnum>::make_test_set(&[TestTwo, TestOne, TestThree, TestFour]);
     
-        assert_eq!(test_tags.none_g(&test_tags), false, "Failed none self test.");
-        assert_eq!(test_tags.none(&[TestTwo, TestOne]), false, "Failed none TestTwo TestOne");
-        assert_eq!(test_tags.none(&[TestTwo]), false, "Failed none TestTwo");
+        assert_eq!(test_tags.contains_none(&test_tags), false, "Failed none self test.");
+        assert_eq!(test_tags.contains_none_slice(&[TestTwo, TestOne]), false, "Failed none TestTwo TestOne");
+        assert_eq!(test_tags.contains_none_slice(&[TestTwo]), false, "Failed none TestTwo");
     
-        assert_eq!(test_tags.none_g(&test_tags2), false, "Failed none test tags 1 V test tags 2");
-        assert_eq!(test_tags2.none_g(&test_tags), false, "Failed none test tags 2 V test tags 1");
-        assert_eq!(test_tags2.none(&[TestTwo, TestOne]), false, "Failed none TestTwo TestOne.");
+        assert_eq!(test_tags.contains_none(&test_tags2), false, "Failed none test tags 1 V test tags 2");
+        assert_eq!(test_tags2.contains_none(&test_tags), false, "Failed none test tags 2 V test tags 1");
+        assert_eq!(test_tags2.contains_none_slice(&[TestTwo, TestOne]), false, "Failed none TestTwo TestOne.");
     
-        assert_eq!(test_tags2.none(&[TestThree]), true, "Failed none third.");
-        assert_eq!(test_tags2.none_g(&test_tags3), true, "Failed none third g.");
+        assert_eq!(test_tags2.contains_none_slice(&[TestThree]), true, "Failed none third.");
+        assert_eq!(test_tags2.contains_none(&test_tags3), true, "Failed none third g.");
+
+        assert_eq!(test_tags4.contains_none(&test_tags3), false, "Failed any fouth g.");
     }
 }
